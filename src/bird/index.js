09523b439e5e33a28b328b5b15703b30b1c35e9a
Anderson Leite <andersonlfl@gmail.com>
2021-11-30 13:43:28 -0800

    bird paralax

---
+++ src/bird/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+var logo = document.querySelector('.logo-header'),
+    blurredBird = document.querySelector('.back-bird'),
+    foreBird = document.querySelector('.fore-bird');
+
+window.addEventListener('scroll', function() {
+  var scrolled = window.scrollY;
+  logo.style.transform = "translate(0px," + scrolled/2 +"%)";
+  blurredBird.style.transform = "translate(0px," + scrolled/5 +"%)";
+  foreBird.style.transform = "translate(0px,-" + scrolled/80 +"%)";
+});
