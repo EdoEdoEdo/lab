ac35a0d267e98ed3d1fd475a23cf5b7a9b484269
Anderson Leite <andersonlfl@gmail.com>
2022-03-16 06:24:40 -0300

    shared workers pt2

--- src/workers3/index.js
+++ src/workers3/index.js
@@ -1,8 +1,17 @@
 import './style.scss';
 
-const worker = new SharedWorker('workers/workers3/worker.js');
+const worker = new SharedWorker('workers/workers3-shared/worker.js');
 
 const log = document.getElementById('log');
 worker.port.onmessage = function(e) { // note: not worker.onmessage!
   log.textContent += '\n' + e.data;
 }
+
+// Shared worker ex 2
+var sharedWorker = new SharedWorker('workers/workers3-shared/shared2.js');
+  var log2 = document.getElementById('log2');
+  sharedWorker.port.addEventListener('message', function(e) {
+    log2.textContent += '\n' + e.data;
+  }, false);
+  sharedWorker.port.start(); // note: need this when using addEventListener
+  sharedWorker.port.postMessage('ping');
