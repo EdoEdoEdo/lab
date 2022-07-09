068a8184903ad820df48ae9161e43f51afee328f
Anderson Leite <andersonlfl@gmail.com>
2022-07-09 18:04:27 +0900

    trail

--- src/spotlight/index.js
+++ src/spotlight/index.js
@@ -1,56 +1,56 @@
 import './style.scss';
 
-import gsap from "gsap";
-import Splitting from "splitting";
-import throttle from "lodash.throttle";
+// import gsap from "gsap";
+// import Splitting from "splitting";
+// import throttle from "lodash.throttle";
 
-const hero = document.querySelector("[data-hero]");
+// const hero = document.querySelector("[data-hero]");
 
 
-/* Cursor */
-const onMouseMove = (e) => {
-  const { clientX, clientY } = e;
-  const x = Math.round((clientX / window.innerWidth) * 100);
-  const y = Math.round((clientY / window.innerHeight) * 100);
+// /* Cursor */
+// const onMouseMove = (e) => {
+//   const { clientX, clientY } = e;
+//   const x = Math.round((clientX / window.innerWidth) * 100);
+//   const y = Math.round((clientY / window.innerHeight) * 100);
 
-  gsap.to(hero, {
-    "--x": `${x}%`,
-    "--y": `${y}%`,
-    duration: 0.3,
-    ease: "sine.out",
-  });
-};
+//   gsap.to(hero, {
+//     "--x": `${x}%`,
+//     "--y": `${y}%`,
+//     duration: 0.3,
+//     ease: "sine.out",
+//   });
+// };
 
-/* Text animation */
-Splitting();
+// /* Text animation */
+// Splitting();
 
-// Set initial text styles (before animation)
-gsap.set(".hero--primary .char", {
-  opacity: 0,
-  y: 25,
-});
+// // Set initial text styles (before animation)
+// gsap.set(".hero--primary .char", {
+//   opacity: 0,
+//   y: 25,
+// });
 
-/* Timeline */
-const tl = gsap.timeline({ delay: 1 });
+// /* Timeline */
+// const tl = gsap.timeline({ delay: 1 });
 
-tl.to(".hero--primary .char", {
-  opacity: 1,
-  y: 0,
-  duration: 0.75,
-  stagger: 0.1,
-})
-  .to(hero, {
-    "--maskSize1": "20%",
-    duration: 0.5,
-    ease: "back.out(2)",
-  })
-  .to(hero, {
-    "--maskSize2": "28%",
-    "--maskSize3": "calc(28% + 0.1rem)",
-    duration: 0.5,
-    delay: 0.3,
-    ease: "back.out(2)",
-  })
-  .then(() => {
-    window.addEventListener("mousemove", throttle(onMouseMove, 30));
-  });
+// tl.to(".hero--primary .char", {
+//   opacity: 1,
+//   y: 0,
+//   duration: 0.75,
+//   stagger: 0.1,
+// })
+//   .to(hero, {
+//     "--maskSize1": "20%",
+//     duration: 0.5,
+//     ease: "back.out(2)",
+//   })
+//   .to(hero, {
+//     "--maskSize2": "28%",
+//     "--maskSize3": "calc(28% + 0.1rem)",
+//     duration: 0.5,
+//     delay: 0.3,
+//     ease: "back.out(2)",
+//   })
+//   .then(() => {
+//     window.addEventListener("mousemove", throttle(onMouseMove, 30));
+//   });
