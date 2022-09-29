a2b515ff90ab888605cf660dfb76015c95eeae39
Anderson Leite <andersonlfl@gmail.com>
2022-09-29 17:11:55 +0900

    char

--- src/gsap-ccc/index.js
+++ src/gsap-ccc/index.js
@@ -55,7 +55,44 @@ const tl03 = gsap.timeline()
 	each:0.02,
 	from:"end"
 }})
-
-
 // GSDevTools.create({animation:tl})
+
+// char by char
+gsap.set(".demo", {opacity:1})
+const chars2 = gsap.utils.toArray("#myText text")
+const tl04 = gsap.timeline()
+.from(chars2, {opacity:0, duration:0.1, stagger:0.05})
+.from(chars2, {scale:0.5, duration:1.2, stagger:0.05, transformOrigin:"50% 50%", ease:"elastic"}, 0)
+
+//squash and jump
+.set(chars2, {transformOrigin:"0% 75%"  })
+.to(chars2, {keyframes:{
+		"20%":{scaleY:0.2, y:0},
+	   "50%":{scaleY:1.3, y:-100, ease:"sine.out"},
+	   "80%":{scaleY:1, y:0, ease:"sine.in"},
+		"82%":{scaleY:0.5, y:0, ease:"sine.out"},
+		"100%":{scaleY:1, y:0, ease:"back(2)"},
+
+	},
+	duration:1,
+	stagger:0.02
+})
+
+
+//skew and remove
+.to(chars2, {skewX:30, x:-20, duration:0.3, ease:"power1.inOut"})
+.to(chars2, {transformOrigin:"50% 50%", skewX:-30, scale:1.5, x:580,ease:"power1.in", duration:0.25,stagger:{
+	each:0.02,
+	from:"end"
+}})
+
+
+GSDevTools.create({animation:tl})
+
+
+
+
+
+
+
 
